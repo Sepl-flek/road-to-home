9a36b260ac9c6796d7263bc7ec18f73f1afca23c
Sepl-flek <154527662+Sepl-flek@users.noreply.github.com>
2024-09-22 14:56:04 +0300

    additing pictures

--- main.cpp
+++ main.cpp
@@ -2,7 +2,27 @@
 
 int main()
 {
- 
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Road to home");
+    sf::Image icon;
+    if (!icon.loadFromFile("pictures/icon.png")) {
+        return 1;
+    }
+    window.setIcon(32, 32, icon.getPixelsPtr());
+
+    sf::Texture textureInfoPanel;
+    textureInfoPanel.loadFromFile("pictures/Panel.png");
+    
+    while (window.isOpen()) 
+    {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+
+    }
 
     return 0;
 }
